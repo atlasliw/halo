6fc249b81045839ccf07bf8951caaa321c3d42d9
jerryvien <jerryyee0902@gmail.com>
2025-04-29 13:35:52 +0800

    Create blank.php

---
+++ blank.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Skydash Admin – Blank Canvas</title>
+
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="vendors/feather/feather.css" />
+  <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css" />
+  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css" />
+  <!-- endinject -->
+
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css" />
+  <link rel="stylesheet" href="js/select.dataTables.min.css" />
+  <!-- End plugin css for this page -->
+
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/vertical-layout-light/style.css" />
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <!-- ... your existing navbar code ... -->
+    </nav>
+    <!-- partial -->
+
+    <div class="container-fluid page-body-wrapper">
+
+      <!-- partial:partials/_settings-panel.html -->
+      <div class="theme-setting-wrapper">…</div>
+      <div id="right-sidebar" class="settings-panel">…</div>
+      <!-- partial -->
+
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <!-- ... your existing sidebar code ... -->
+      </nav>
+      <!-- partial -->
+
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <!-- ================= Blank Canvas Area ================= -->
+          <div class="row">
+            <div class="col-12">
+              <!-- Empty for your DataTable or other content -->
+            </div>
+          </div>
+          <!-- ===================================================== -->
+        </div>
+        <!-- content-wrapper ends -->
+
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">
+              Copyright © 2021. Premium
+              <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a>
+              from BootstrapDash. All rights reserved.
+            </span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+              Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i>
+            </span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+
+  <!-- Plugin js for this page -->
+  <script src="vendors/chart.js/Chart.min.js"></script>
+  <script src="vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="js/dataTables.select.min.js"></script>
+  <!-- End plugin js for this page -->
+
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+  <script src="js/settings.js"></script>
+  <script src="js/todolist.js"></script>
+  <!-- endinject -->
+
+  <!-- Custom js for this page-->
+  <!-- (empty for now) -->
+  <!-- End custom js for this page-->
+</body>
+
+</html>
